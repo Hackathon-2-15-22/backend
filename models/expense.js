c40de39008f52d4bc788a5b2bb2dfa907df48798
Angela Quach <angelayunquach@gmail.com>
2022-02-15 17:05:27 -0500

    add expense model

---
+++ models/expense.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const categorySchema = new Schema({
+  name: {
+    type: String, 
+    required: true,
+    lowercase: true, 
+    unique: true
+  }
+})
+
+const expenseSchema = new Schema({
+  name: {
+    type: Number,
+  },
+  amount: {
+    type: Number,
+  },
+  category: [categorySchema],
+})
+
+const Expense = mongoose.model("Expense", expenseSchema)
+
+export {
+  Expense
+}
--- models/profile.js
+++ models/profile.js
@@ -4,6 +4,8 @@ const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
   income: [{type: mongoose.Schema.Types.ObjectId, ref: "Income"}],
+  expense: [{type: mongoose.Schema.Types.ObjectId, ref: "Expense"}],
+  goal: [{type: mongoose.Schema.Types.ObjectId, ref: "Goal"}],
 },{
     timestamps: true,
 })
