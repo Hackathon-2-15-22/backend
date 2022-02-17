73834877e71486460b41518d19780eaf4aa89e79
Jan Horak <janny_b_goode@hotmail.com>
2022-02-16 23:29:54 -0500

    add income index route and control function

--- routes/incomes.js
+++ routes/incomes.js
@@ -9,6 +9,7 @@ const router = Router()
 
 // ========= Protected Routes ========= 
 router.use(decodeUserFromToken)
+router.get('/', checkAuth, incomeCtrl.index)
 router.post('/', checkAuth, incomeCtrl.create)
 router.put('/:id', checkAuth, incomeCtrl.update)
 router.delete('/:id', checkAuth, incomeCtrl.delete)
